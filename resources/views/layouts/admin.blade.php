61bdb25995a2eaff9fe7100adb485334711cea85
Abdul Sumail <39582191+MrKappah@users.noreply.github.com>
2018-09-27 16:12:57 +0200

    Alteracoes do layouts

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
     <head>
         
         <!-- Title -->
-        <title>Modern | Admin Dashboard Template</title>
+        <title>N'Contabilidade</title>
         
         <meta content="width=device-width, initial-scale=1" name="viewport"/>
         <meta charset="UTF-8">
