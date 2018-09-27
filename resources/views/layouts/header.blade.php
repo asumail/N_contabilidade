4d08eeb7ca6b6da6260096ac548e824919636c5d
Abdul Sumail <39582191+MrKappah@users.noreply.github.com>
2018-09-27 15:56:49 +0200

    sem stress

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <div class="menu-wrap">
         <nav class="profile-menu">
-            <div class="profile"><img src="assets/images/profile-menu-image.png" width="60" alt="David Green"/><span>David Green</span></div>
+            <div class="profile"><img src="assets/images/profile-menu-image.png" width="60" alt="David Green"/><span></span></div>
             <div class="profile-menu-list">
                 <a href="#"><i class="fa fa-star"></i><span>Favorites</span></a>
                 <a href="#"><i class="fa fa-bell"></i><span>Alerts</span></a>
