5cb6f168c95437cee2984ba4443b2afbf687639a
Abdul Sumail <39582191+MrKappah@users.noreply.github.com>
2018-09-27 10:31:07 +0200

    Criacao dos files para Layouts 'admin' ,'sidebar', 'footer','header','welcome'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    N'Contabilidade
                 </div>
 
                 <div class="links">
