e861032fbdde8e2d80ec8e40a53f9e7af719ac68
machiza <machizaarmando@gmail.com>
2018-09-28 08:28:54 +0200

    model e migration da 'Sub_Empresa

---
+++ database/migrations/2018_09_28_062749_create_sub_empresas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_empresas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('aliase');
+            $table->string('localizacao');
+            $table->string('celular');
+            $table->string('fixo');
+            $table->foreign('empresa_id')
+                 ->references('id')->on('empresas')
+                 ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_empresas');
+    }
+}
