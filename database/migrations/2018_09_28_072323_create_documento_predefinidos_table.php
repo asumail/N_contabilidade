7f86295c5bbb9a74be9aee5afde840b0eb5330ff
Abdul Sumail <39582191+MrKappah@users.noreply.github.com>
2018-09-28 11:30:17 +0200

    Criacao dos 'models' e 'migrations' 'anoContabilistico','Diario','DocumentoPredefinido','Lancamento','PlanoConta',''

---
+++ database/migrations/2018_09_28_072323_create_documento_predefinidos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocumentoPredefinidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documento_predefinidos', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('sub_empresa_id');
+            $table->foreign('sub_empresa_id')
+                 ->references('id')->on('sub_empresas')
+                 ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documento_predefinidos');
+    }
+}
