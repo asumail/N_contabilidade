7ac8efb7e690fd9631f66dad39cf6b60d41d21ee
Abdul Sumail <39582191+MrKappah@users.noreply.github.com>
2018-09-28 13:59:03 +0200

    Adicionando a estrutura aos formularios da Sub_Empresa 'homeView','createView','indexView'

---
+++ resources/views/sub_empresa/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.admin')
+
+
+<!--Css Local-->
+@section('cssLocal')
+
+@endsection
+
+
+<!--Conteudo Local-->
+@section('content')
+
+
+@endsection
+<!--Fim do Conteudo Local-->
+
+<!--Javascript Local-->
+@section('scripts')
+
+@endsection
+<!-- fim Javascript Local-->
