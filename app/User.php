73dc4937de5519d21e9bfd43c8f3f9c11c44ebc5
machiza <machizaarmando@gmail.com>
2018-10-01 14:01:17 +0200

    niveis de permissao

--- app/User.php
+++ app/User.php
@@ -13,6 +13,36 @@ class User extends Authenticatable
         return $this->belongsToMany('App\Role');
     }
 
+    public function authorizeRoles($roles) {
+        if($this->hasAnyRole($roles)) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    public function hasAnyRole($roles) {
+        if(is_array($roles)) {
+            foreach ($roles as $role) {
+                if($this->hasRole($role)) {
+                    return true;
+                }
+            }
+        } else {
+            if($this->hasRole($role)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function hasRole($role) {
+        if($this->roles()->where('name',$role)->first()) {
+            return true;
+        }
+        return false;
+    }
     /**
      * The attributes that are mass assignable.
      *
