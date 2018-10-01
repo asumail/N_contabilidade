cb8fce5ad7529ab6e674481bcc21355c3dd3a531
machiza <machizaarmando@gmail.com>
2018-10-01 14:53:40 +0200

    Metodo 'index' e 'create' do EmpresaController

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -13,7 +13,7 @@ class EmpresaController extends Controller
      */
     public function index()
     {
-        //
+        return view('empresa.index');
     }
 
     /**
@@ -23,7 +23,7 @@ class EmpresaController extends Controller
      */
     public function create()
     {
-        //
+        return view('empresa.create');
     }
 
     /**
